7333f1cb7d0234df4179da331b3999cb83b9f68a
aalorro <aalorro@gmail.com>
2026-03-04 09:22:45 -0500

    style name issue on canvas

--- src/components/CanvasRenderer.tsx
+++ src/components/CanvasRenderer.tsx
@@ -473,7 +473,7 @@ export const CanvasRenderer: React.FC<CanvasRendererProps> = ({ showFPS = false 
       )}
 
       {/* Bottom Canvas Buttons */}
-      <div className="absolute bottom-24 left-1/2 -translate-x-1/2 z-30 flex flex-col items-center gap-2">
+      <div className="absolute bottom-4 lg:bottom-24 left-1/2 -translate-x-1/2 z-30 flex flex-col items-center gap-2">
         <div className="flex gap-3">
           <button
             onClick={undo}
